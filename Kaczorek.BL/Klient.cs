3929c7d3c2ccf756439826b0447ab3e1bbec56b3
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-11-05 09:14:55 +0100

    3_4 Budowanie klasy Klienta - własciwosci

---
+++ Kaczorek.BL/Klient.cs
@@ -0,0 +1,35 @@
+﻿namespace Kaczorek.BL
+{
+    public class Klient
+    {
+        private string _nazwisko;
+
+        public string Nazwisko
+        {
+            get
+            {
+                // tutaj możemy dodac dowolny kod
+                return _nazwisko;
+            }
+            set
+            {
+                // tutaj możemy dodac dowolny kod
+                _nazwisko = value;
+            }
+        }
+
+        public string Imie { get; set; }
+
+        public string Email { get; set; }
+
+        public int KlientId { get; private set; }
+
+        public string ImieNazwisko
+        {
+            get
+            {
+                return Imie + "," + Nazwisko;
+            }
+        }
+    }
+}
