5a9b5e3aa9ff5eb296cb71da1829e108504560f0
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-11-06 08:56:23 +0100

    4_5 Budowanie pozostałych klas

---
+++ Kaczorek.BL/PozycjaZamowienia.cs
@@ -0,0 +1,60 @@
+﻿namespace Kaczorek.BL
+{
+    public class PozycjaZamowienia
+    {
+        public PozycjaZamowienia()
+        {
+
+        }
+
+        public PozycjaZamowienia(int pozycjaZmowieniaId)
+        {
+            PozycjaZmowieniaId = pozycjaZmowieniaId;
+        }
+
+        public int PozycjaZmowieniaId { get; private set; }
+        public int Ilosc { get; set; }
+        public int ProduktId { get; set; }
+        public decimal? CenaZakupu { get; set; }
+
+        // Metody
+        /// <summary>
+        /// Sprawdza dane pozycji zamowienia
+        /// </summary>
+        /// <returns></returns>
+        public bool Zwalduj()
+        {
+            var poprawne = true;
+
+            if (Ilosc <= 0)
+                poprawne = false;
+            if (ProduktId <= 0)
+                poprawne = false;
+            if (CenaZakupu == 0)
+                poprawne = false;
+
+            return poprawne;
+        }
+
+        /// <summary>
+        /// Zapisuje element zamowienie
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // kod, który zapisuje zdefiniowane zamowienie
+            return true;
+        }
+
+        /// <summary>
+        /// Pobieramy jedną element zamowienia
+        /// </summary>
+        /// <param name="pozycjaZamowieniaId"></param>
+        /// <returns></returns>
+        public PozycjaZamowienia Pobierz(int pozycjaZamowieniaId)
+        {
+            //Kod który pobiera określony produkt
+            return new PozycjaZamowienia();
+        }
+    }
+}
---
+++ Kaczorek.BL/Zamowienie.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Kaczorek.BL
+{
+    public class Zamowienie
+    {
+        public Zamowienie()
+        {
+
+        }
+
+        public Zamowienie(int zamowienieId)
+        {
+            ZamowienieId = zamowienieId;
+        }
+
+        public int ZamowienieId { get; private set; }
+        public DateTimeOffset? DataZamowienia { get; set; }
+
+        // Metody
+        /// <summary>
+        /// Sprawdza dane zamowienia
+        /// </summary>
+        /// <returns></returns>
+        public bool Zwalduj()
+        {
+            var poprawne = true;
+
+            if (DataZamowienia == null)
+                poprawne = false;
+
+            return poprawne;
+        }
+
+        /// <summary>
+        /// Zapisuje zamowienie
+        /// </summary>
+        /// <returns></returns>
+        public bool Zapisz()
+        {
+            // kod, który zapisuje zdefiniowane zamowienie
+            return true;
+        }
+
+        /// <summary>
+        /// Pobieramy jedno zamowienie
+        /// </summary>
+        /// <param name="zamowienieId"></param>
+        /// <returns></returns>
+        public Zamowienie Pobierz(int zamowienieId)
+        {
+            //Kod który pobiera określony produkt
+            return new Zamowienie();
+        }
+
+        /// <summary>
+        /// Pobieramy wszystkie zamowienia
+        /// </summary>
+        /// <returns></returns>
+        public List<Zamowienie> Pobierz()
+        {
+            //Kod który pobiera wszystkie zamowienia
+            return new List<Zamowienie>();
+        }
+    }
+}
