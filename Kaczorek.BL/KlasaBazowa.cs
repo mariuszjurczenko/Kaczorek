578eeb99d79dcabbb5785dcc4652e9ed75c944a9
mariusz jurczenko <mariuszjurczenko@gmail.com>
2018-11-08 12:09:02 +0100

    7_6 klasa bazowa

---
+++ Kaczorek.BL/KlasaBazowa.cs
@@ -0,0 +1,23 @@
+﻿namespace Kaczorek.BL
+{
+    public abstract class KlasaBazowa
+    {
+        public bool JestNowy { get; set; }
+        public bool MaZmiany { get; set; }
+        public StanObiektuOpcje StanObiektu { get; set; }
+
+        public bool DanePrawidlowe
+        {
+            get
+            {
+                return true;
+            }
+        }
+    }
+
+    public enum StanObiektuOpcje
+    {
+        Atywny,
+        Usuniety
+    }
+}
